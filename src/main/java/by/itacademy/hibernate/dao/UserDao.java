27b703f342fa51509a8e9e0a1bc63b26abaac72a
breadKozlov <killersarequiet2018@gmail.com>
2023-05-11 09:51:24 +0300

    Add test

--- src/main/java/by/itacademy/hibernate/dao/UserDao.java
+++ src/main/java/by/itacademy/hibernate/dao/UserDao.java
@@ -19,28 +19,33 @@ public class UserDao {
      * Возвращает всех сотрудников
      */
     public List<User> findAll(Session session) {
-        return Collections.emptyList();
+        return session.createQuery("FROM User",User.class).list();
     }
 
     /**
      * Возвращает всех сотрудников с указанным именем
      */
     public List<User> findAllByFirstName(Session session, String firstName) {
-        return Collections.emptyList();
+
+        return session.createQuery("FROM User U WHERE U.personalInfo.firstname = :firstName",User.class)
+                .setParameter("firstName",firstName).list();
     }
 
     /**
      * Возвращает первые {limit} сотрудников, упорядоченных по дате рождения (в порядке возрастания)
      */
     public List<User> findLimitedUsersOrderedByBirthday(Session session, int limit) {
-        return Collections.emptyList();
+        return session.createQuery("FROM User U ORDER BY U.personalInfo.birthDate asc",User.class)
+                .setMaxResults(limit).list();
     }
 
     /**
      * Возвращает всех сотрудников компании с указанным названием
      */
     public List<User> findAllByCompanyName(Session session, String companyName) {
-        return Collections.emptyList();
+
+        return session.createQuery("FROM User U WHERE U.company.name = :name",User.class)
+                .setParameter("name",companyName).list();
     }
 
     /**
@@ -48,21 +53,30 @@ public class UserDao {
      * упорядоченные по имени сотрудника, а затем по размеру выплаты
      */
     public List<Payment> findAllPaymentsByCompanyName(Session session, String companyName) {
-        return Collections.emptyList();
+        return session.createQuery("FROM Payment P WHERE P.receiver.company.name = :name" +
+                " ORDER BY P.receiver.personalInfo.firstname asc, P.amount asc",Payment.class)
+                .setParameter("name",companyName).list();
     }
 
     /**
      * Возвращает среднюю зарплату сотрудника с указанными именем и фамилией
      */
     public Double findAveragePaymentAmountByFirstAndLastNames(Session session, String firstName, String lastName) {
-        return Double.NaN;
+        var result = session.createQuery("SELECT P.receiver.personalInfo.firstname as firstname," +
+                " P.receiver.personalInfo.lastname as lastname, avg(P.amount) FROM Payment P " +
+                "WHERE firstname = :firstname AND lastname = :lastname" +
+                " GROUP BY firstname,lastname",Object[].class)
+                .setParameter("firstname",firstName)
+                .setParameter("lastname",lastName).list().stream().findFirst().orElseThrow();
+        return (Double) result[2];
     }
 
     /**
      * Возвращает для каждой компании: название, среднюю зарплату всех её сотрудников. Компании упорядочены по названию.
      */
     public List<Object[]> findCompanyNamesWithAvgUserPaymentsOrderedByCompanyName(Session session) {
-        return Collections.emptyList();
+        return session.createQuery("select rec.company.name as name,avg (P.amount)" +
+                " from Payment P join P.receiver as rec join rec.company group by name order by name",Object[].class).list();
     }
 
     /**
@@ -71,7 +85,11 @@ public class UserDao {
      * Упорядочить по имени сотрудника
      */
     public List<Object[]> isItPossible(Session session) {
-        return Collections.emptyList();
+
+        var allAvg = session.createQuery("select avg(P.amount) from Payment P").getSingleResult();
+        return session.createQuery("select user,avg(P.amount) from Payment P join P.receiver as user " +
+                        "group by user.id having avg(P.amount) > :allAvg order by P.receiver.personalInfo.firstname asc",Object[].class)
+                .setParameter("allAvg",allAvg).list();
     }
 
     public static UserDao getInstance() {
--- src/main/java/by/itacademy/hibernate/entity/Birthday.java
+++ src/main/java/by/itacademy/hibernate/entity/Birthday.java
@@ -3,8 +3,14 @@ package by.itacademy.hibernate.entity;
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 
-public record Birthday(LocalDate birthDate) {
+public record Birthday(LocalDate birthDate) implements Comparable<Birthday> {
     public long getAge() {
         return ChronoUnit.YEARS.between(birthDate, LocalDate.now());
+    }
+
+
+    @Override
+    public int compareTo(Birthday o) {
+        return birthDate.compareTo(o.birthDate);
     }
 }
